12f1e6da065380de887570907003fde1b88cae3e
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-06-07 01:02:37 -0700

    phasegate

---
+++ PYTHON QUESTION/test_cycle.py
@@ -0,0 +1,19 @@
+import cycle
+from unittest import TestCase
+
+
+class TestCycle(TestCase):
+
+    def test_that_calculate_menstrual_cycle_function_exists(self):
+        cycle.calculate_menstrual_cycle(5, 28)
+
+    def test_that_function_returns_correct_values(self):
+        actual = cycle.calculate_menstrual_cycle(5, 28)
+        expected = (33, 19, 17, 21, 14, 24)
+        self.assertEqual(actual, expected)
+
+        actual = cycle.calculate_menstrual_cycle(10, 30)
+        expected = (40, 26, 24, 28, 21, 31)
+        self.assertEqual(actual, expected)
+
+    
