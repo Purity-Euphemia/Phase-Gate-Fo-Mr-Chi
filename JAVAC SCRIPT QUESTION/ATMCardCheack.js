4816c6892d7d302bd8fb77db447b49f738eaa39d
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-06-10 09:40:19 -0700

    Add files via upload

---
+++ JAVAC SCRIPT QUESTION/ATMCardCheack.js
@@ -0,0 +1,74 @@
+function ATMCardCheack(array) {
+  let mastercard = {
+    valid: true,
+    issuer: "Mastercard",
+  };
+
+  let discover = {
+    valid: true,
+    issuer: "Discover",
+  };
+
+  let AmericaExpress = {
+    valid: true,
+    issuer: "American Express",
+  };
+
+  let visa = {
+    valid: true,
+    issuer: "Visa",
+  };
+
+  let invalid = {
+    valid: false,
+    issuer: "Invalid",
+  };
+
+  
+  let totalStep2 = 0; // Doubled values
+  let totalStep3 = 0; // Odd positions (not doubled)
+  let length = array.length;
+
+  // Step 1 + 2: Double every second digit from the right and fix if needed
+  for (let i = length - 2; i >= 0; i -= 2) {
+    let digit = array[i];
+    let doubled = digit * 2;
+
+    if (doubled > 9) {
+	doubled = doubled - 9;
+    }
+
+    totalStep2 += doubled;
+  }
+
+  // Step 3: Add the remaining digits (those not doubled)
+  for (let i = length - 1; i >= 0; i -= 2) {
+    totalStep3 += array[i];
+  }
+
+ 
+  let total = totalStep2 + totalStep3;
+
+  // Step 5: Check validity
+  let isValid = total % 10 === 0;
+
+  if (!isValid) {
+    return invalid;
+  }
+
+ 
+  if (array[0] === 5 && array.length === 16) return mastercard;
+  if (array[0] === 6 && array.length === 16) return discover;
+  if (array[0] === 3 && array[1] === 7 && array.length === 15) return AmericaExpress;
+  if (array[0] === 4 && (array.length === 13 || array.length === 16)) return visa;
+
+  return valid;
+}
+
+
+
+let two = [5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 4, 4, 4];
+let three = [6, 5, 6, 7, 8, 9, 2, 3, 7, 3, 5, 1, 9, 4, 2, 7];
+
+let result = ATMCardCheack(two);
+console.log(result);
