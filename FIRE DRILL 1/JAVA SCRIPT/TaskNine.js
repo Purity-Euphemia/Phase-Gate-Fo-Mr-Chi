961cb9e3fe4e96c393dcdf14583d7a4215e4119f
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-06-17 21:35:46 -0700

    Add files via upload

---
+++ FIRE DRILL 1/JAVA SCRIPT/TaskNine.js
@@ -0,0 +1,15 @@
+const number = 10; 
+	
+	let total = 0;
+	for (let count = 1; count <= number; count++) {
+		if (count % 4 == 0) {
+			let sum = 0;
+	for(let counter = 1; counter <= 5; counter++){	
+		sum += (Math.floor(Math.pow(count, counter)));
+}
+	total += sum;		
+}
+}	
+
+	console.log (total * total);
+
